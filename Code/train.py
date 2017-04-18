e664dabcd9b82fd021999a6fd6f59725056de0b5
zhuangdizhu <zhuangdizhu@yahoo.com>
2017-04-17 23:13:57 -0400

    local change

--- Code/train.py
+++ Code/train.py
@@ -35,7 +35,7 @@ def main():
                        help='learning rate')
     parser.add_argument('--decay_rate', type=float, default=0.97,
                        help='decay rate for rmsprop')
-    parser.add_argument('--dropout', type=float, default=0.8,
+    parser.add_argument('--dropout', type=float, default=1,
                         help='keep out rate for rnn')
     parser.add_argument('--init_from', type=str, default=None,
                        help="""continue training from saved model at this path. Path must contain files saved by previous training process:
